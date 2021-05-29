e25917d711577b212ad89c314b938f6e26aa13ca
Fabio Leardini <fabioleardini@gmail.com>
2021-05-29 18:01:49 -0300

    first commit

---
+++ solid/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace solid
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
